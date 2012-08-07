5db16555ef6dffebfed192e946187ad0a8e89068
Izzmo <me@izzmo.com>
2012-08-07 15:09:53 -0500

    - Updated readme. - Added JavaScript file.

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-AutoAwesomer
-============
+#Izzmo's Turntable.fm Auto Awesomer
+![Izzmo's AutoAwesome](http://www.pinnacleofdestruction.net/tt/images/aa-small.png)
 
-A Turntable.fm bookmarklet that automatically awesomes every song for you.
+This script allows you to enjoy turntable.fm music without getting paranoid about voting for every song, which you should be doing! Instead, forget the hassle and turn this on to have the songs you listen to get voted awesome automatically!
+
+If you do not like a song, simply go back to the turntable.fm room tab and click lame, and the counter will not resume until the next song!
+
+## Features
+
+* Automatically votes awesome for every song you listen to in your turntable.fm room.
+* Allows for song laming. If you don't like a song, this script isn't like others. It will see that you lame'd a song and wait till the next song to start awesomering again.
+* Never go AFK! This script will keep you active in the room; so if you are working while DJ'ing, you can be sure you will never get booted for being AFK.
+* No more idle boot! If you have ever sat in a room too long (usually around 2-3 hours) without voting, you will get booted back to the lobby for being AFK. No more! This script resets your idle timer
+
+## Download
+
+In order to use the AutoAwesomer, please visit our website at http://izzmo.com/tt/ for instructions on how to add it to your bookmarks bar and get it up and running.
+
+## Changelog
+
+### March 26th, 2012
+Fixed a bug not allowing the auto-awesome to start.
+
+### March 22nd, 2012
+Fixed a bug that would not cause the script to load due to my interface extension running.
+
+### February 17th, 2012
+Extended the time to check for interface changes (when changing rooms) so the AA interface comes back up.
+
+### February 13th, 2012
+Added in some extra validation to make sure the awesome arc was not going passed 180 degrees.
+
+### February 12th, 2012
+* Room Switching is now supported. If you move to another room on turntable, the AutoAwesome will stay intact!
+* If you are DJ'ing and currently playing a song, the AA will no longer try to awesome that song.
+
+### February 11th, 2012
+* Fixed bug where AutoAwesome would not start. Please email me or chat with me if this is still happening to you!
+* Added a awesome arc. This is much like a counter, but graphical instead of raw numbers. When it reaches the other side of the vote meter, it awesomes the song.
+* Added visual feedback. If you awesome the song before the countdown is up, it will stop and turn green; or if you lame it at any point, it will stop the countdown and the arc will turn bright red.
+
+### January 23rd, 2012
+* Added support for laming a song and canceling the auto awesome for the song. (Support for this was taken out of the beta build but re-added shortly after release).
+* Awesome once per song. Now the AutoAwesome only awesomes the song once per song instead of multiple times with the countdown method.
+* No more countdown! The simplified interface will hopefully answer some peoples questions the first time they operate the AutoAwesome and wonder what the countdown is.
+* Added support so if you click your bookmark multiple times, only the first time will start the bookmark. This prevents you from getting weird countdowns and causing multiple awesomes for each song.
+
+### January 10th, 2012
+Fixed a problem with the AFK timer not working.
+
+### December 13th, 2011
+Updated some core functions so they do not rely on timers anymore, but more of Turntable's default functionality.
+
+### December 6th, 2011
+* Movable! You can now move the button wherever you would like on the screen. Note: In Firefox, it is a little buggy. To move it, you must select the part of the circle that is not a link (hand cursor) or else when you stop moving it, it will close it.
+* The timer button (green circle) now shows in top left corner.
+* Bug Fix: Some users were experiencing a "callback reference" error.
+
+### December 5th, 2011
+Better lame support. When you lame a song with the AA script running, it will not suspend the timer and resume upon the next song so a song you lame stays lamed.
---
+++ auto-awesome.js
@@ -0,0 +1,226 @@
+/**
+ * Turntable.fm Auto Awesome Script
+ * Created by Izzmo, http://ttaa.codeplex.com
+ * Last Updated: March 22nd, 2012
+ * 
+ * If you have any questions or concerns,
+ * please email me: me@izzmo.com, or find
+ * me in http://tt.fm/dubstep
+ */
+
+$(document).ready(function() {
+  if(window.izzmo == undefined) window.izzmo = { };
+  
+  window.izzmo = $.extend(window.izzmo, {
+    ttObj: null,
+    awesomer: null,
+    showArc: true,
+    lamed: false,
+    arcInt: 0,
+    deg: 0.0,
+    vote: function(c) {
+      var f = $.sha1(window.izzmo.ttObj.roomId + c + window.izzmo.ttObj.currentSong._id);
+      var d = $.sha1(Math.random() + "");
+      var e = $.sha1(Math.random() + "");
+      window.izzmo.socket({
+          api: "room.vote",
+          roomid: window.izzmo.ttObj.roomId,
+          val: c,
+          vh: f,
+          th: d,
+          ph: e
+      });
+    },
+    socket: function (c, a) {
+        if (c.api == "room.now") {
+            return;
+        }
+        c.msgid = turntable.messageId;
+        turntable.messageId += 1;
+        c.clientid = turntable.clientId;
+        if (turntable.user.id && !c.userid) {
+            c.userid = turntable.user.id;
+            c.userauth = turntable.user.auth;
+        }
+        var d = JSON.stringify(c);
+        if (turntable.socketVerbose) {
+            LOG(util.nowStr() + " Preparing message " + d);
+        }
+        var b = $.Deferred();
+        turntable.whenSocketConnected(function () {
+            if (turntable.socketVerbose) {
+                LOG(util.nowStr() + " Sending message " + c.msgid + " to " + turntable.socket.host);
+            }
+            if (turntable.socket.transport.type == "websocket") {
+                turntable.socketLog(turntable.socket.transport.sockets[0].id + ":<" + c.msgid);
+            }
+            turntable.socket.send(d);
+            turntable.socketKeepAlive(true);
+            turntable.pendingCalls.push({
+                msgid: c.msgid,
+                handler: a,
+                deferred: b,
+                time: util.now()
+            });
+        });
+        return b.promise();
+    },
+    listener: function(d) {
+      if(d.command == "newsong" && d.room.metadata.current_dj != izzmo.ttObj.selfId) {
+        clearTimeout(window.izzmo.awesomer);
+        clearInterval(window.izzmo.arcInt);
+        window.izzmo.lamed = false;
+        var timeAmt = Math.floor(Math.random()*window.izzmo.ttObj.currentSong.metadata.length/2*1000);
+        window.izzmo.botMessage.find('span').html('');
+        window.izzmo.awesomer = setTimeout(function() {
+          window.izzmo.vote('up');
+        }, timeAmt);
+        
+        if(!window.izzmo.showArc) return;
+
+        window.izzmo.deg = 0.0;
+        window.izzmo.degAmt = 180 / timeAmt * 55;
+        if(window.izzmo.arcInt != 0) {
+          clearInterval(window.izzmo.arcInt);
+          window.izzmo.arcInt = 0;
+        }
+        window.izzmo.arcInt = setInterval(function() {
+          if(window.izzmo.deg >= 180) {
+            clearInterval(window.izzmo.arcInt);
+            window.izzmo.arcInt = 0;
+          }
+          window.izzmo.setArc(window.izzmo.deg, false);
+          window.izzmo.deg += window.izzmo.degAmt;
+        }, 50);
+      }
+    },
+    setArc: function(degree, red) {
+      if(!window.izzmo.showArc) return;
+      var context = window.izzmo.arc[0].getContext('2d');
+      context.clearRect(0, 0, 1000, 500);
+      context.beginPath();
+      context.arc(203, 162, 137, -Math.PI, degree*Math.PI/180 - Math.PI, false);
+      context.lineWidth = 5;
+      if(red)
+        context.strokeStyle = 'rgb(255, 0, 0)';
+      else
+        context.strokeStyle = 'rgb(0, 200, 0)';
+      context.shadowOffsetX = 0;
+      context.shadowOffsetY = 0;
+      context.shadowBlur = 10;
+      if(red)
+        context.shadowColor = 'rgba(255, 0, 0, 1)';
+      else
+        context.shadowColor = 'rgba(0, 255, 0, 1)';
+      context.stroke();
+    },
+    room: '',
+    watcher: null,
+    stop: function() {
+      clearTimeout(window.izzmo.awesomer);
+      clearInterval(window.izzmo.arcInt);
+      window.izzmo.arcInt = 0;
+    },
+    init: function() {
+      $('.roomView').ready(function() {
+        for(var prop in window.turntable) {
+          if(window.turntable[prop] != undefined && window.turntable[prop].hasOwnProperty('currentDj'))
+            window.izzmo.ttObj = window.turntable[prop];
+        }
+        if(window.izzmo.ttObj === null) {
+          alert('Could not find turntable.fm objects. You should refresh your page and try again.');
+          return;
+        }
+        window.izzmo.room = window.location.pathname;
+        var meterObj = $('#meterGauge');
+        if(meterObj.length > 0 && meterObj.css('display') != 'none') {
+          var meter = $('#meterGauge').position();
+          window.izzmo.arc = $('<canvas id="izzmo-arc" width="406" height="158" style="overflow: hidden; position: absolute; z-index: 20000; top: ' + meter.top + 'px; left: ' + meter.left + 'px;">Izzmo\'s AutoAwesome</canvas>');
+          $($('.roomView > div')[1]).prepend(window.izzmo.arc);
+          window.izzmo.showArc = true;
+        }
+        else
+          window.izzmo.showArc = false;
+
+        window.izzmo.botMessage = $('<div id="bot-message">Izzmo\'s AutoAwesome. <span style="font-style: italic;"></span> <a href="#" style="text-decoration: none; color: red; font-weight: bold;">Turn off</a></div>');
+        window.izzmo.botMessage.css({
+          position: 'fixed',
+          color: 'white',
+          top: '0px',
+          zIndex: '5000',
+          textAlign: 'left',
+          paddingLeft: '2px',
+          paddingTop: '2px',
+          paddingRight: '3px',
+          paddingBottom: '2px',
+          fontSize: '10px',
+          fontFace: 'Verdana'
+        });
+
+        if(!$('#bot-message').length) {
+          $('.header').append(window.izzmo.botMessage);
+
+          window.izzmo.botMessage.find('a').click(function(e) {
+            e.preventDefault();
+            window.izzmo.destruct();
+            window.turntable.removeEventListener("message", window.izzmo.listener);
+            window.izzmo = null;
+          });
+        }
+
+        var buttons = $('.roomView > div:nth-child(2) a[id]'); // 1st is Awesome button, 2nd is Lame
+        $(buttons[1]).unbind(); // cancel TT's default callback for the button, add in our own.
+        $(buttons[1]).bind('click', function() {
+          if(!window.izzmo.lamed) {
+            window.izzmo.vote('up');
+            window.izzmo.stop();
+            window.izzmo.botMessage.find('span').html("Song lamed! Awesomering will resume next song.");
+            window.izzmo.lamed = true;
+          }
+          window.izzmo.setArc(180, true);
+          setTimeout(function() {window.izzmo.vote('down');}, 250);
+        });
+        $(buttons[0]).bind('click', function() {
+            window.izzmo.stop();
+            window.izzmo.setArc(180, false);    
+        });
+
+        turntable.addEventListener("message", window.izzmo.listener);
+        window.izzmo.vote('up');
+        window.izzmo.setArc(180, false);
+
+        // Timer for resetting TurnTable's AFK Timers
+        // Runs every 60 seconds
+        window.izzmo.botResetAFKTimer = setInterval(function() {
+          $($('form input:last')[0]).keydown();
+        }, 60000);
+
+        window.izzmo.watcher = setInterval(function() {
+          if(window.location.pathname != window.izzmo.room) {
+            console.log('New Room found, reinitializing...');
+            window.izzmo.destruct();
+            if(window.izzmo.showArc) {
+              var meterObj = $('#meterGauge');
+              var check = setInterval(function() {
+                if(meterObj.length > 0 && meterObj.css('display') != 'none') {
+                  window.izzmo.init();
+                  clearInterval(check);
+                }
+              }, 1000);
+              setTimeout(function() {clearInterval(check);}, 10000);
+            }
+          }
+        }, 3000);
+      });
+    },
+    destruct: function() {
+      clearInterval(window.izzmo.botResetAFKTimer);
+      clearInterval(window.izzmo.watcher);
+      window.izzmo.stop();
+      window.izzmo.arc.remove();
+      window.izzmo.botMessage.remove();
+    }
+  });
+  
+  window.izzmo.init();
+});
